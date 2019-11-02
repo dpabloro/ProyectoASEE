ec50527ff88c0d53115ac5e569a8f1735a6d82c2
emartinhz <emartinhz@alumnos.unex.es>
2019-11-02 20:29:42 +0100

    Creacion de to_do_items, content_item, y AddActivity. Generacion de dimens.xml

---
+++ app/src/main/java/es/unex/giiis/asee/proyectoasee/AddActivity.java
@@ -0,0 +1,10 @@
+package es.unex.giiis.asee.proyectoasee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddActivity extends AppCompatActivity {
+
+
+
+
+}
--- app/src/main/java/es/unex/giiis/asee/proyectoasee/MainActivity.java
+++ app/src/main/java/es/unex/giiis/asee/proyectoasee/MainActivity.java
@@ -13,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+
+
+
+
 }
